828ea2f21031fed22511de0e517444502a48a81e
toddr <todd.rudberg@icloud.com>
2024-10-29 05:50:58 -0700

    continued

--- SportClassAnalyzer/frmMain.Designer.cs
+++ SportClassAnalyzer/frmMain.Designer.cs
@@ -30,41 +30,32 @@
         {
             listBox1 = new ListBox();
             listBox2 = new ListBox();
-            webView2Control = new Microsoft.Web.WebView2.WinForms.WebView2();
             menuStrip1 = new MenuStrip();
             fileToolStripMenuItem = new ToolStripMenuItem();
             openToolStripMenuItem = new ToolStripMenuItem();
             openRaceBoxFileToolStripMenuItem = new ToolStripMenuItem();
-            ((System.ComponentModel.ISupportInitialize)webView2Control).BeginInit();
             menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
-            listBox1.Location = new Point(27, 40);
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(34, 50);
+            listBox1.Margin = new Padding(4, 4, 4, 4);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(436, 344);
+            listBox1.Size = new Size(544, 429);
             listBox1.TabIndex = 0;
             // 
             // listBox2
             // 
             listBox2.FormattingEnabled = true;
-            listBox2.Location = new Point(483, 40);
+            listBox2.ItemHeight = 25;
+            listBox2.Location = new Point(604, 50);
+            listBox2.Margin = new Padding(4, 4, 4, 4);
             listBox2.Name = "listBox2";
-            listBox2.Size = new Size(854, 344);
+            listBox2.Size = new Size(1066, 429);
             listBox2.TabIndex = 1;
-            // 
-            // webView2Control
-            // 
-            webView2Control.AllowExternalDrop = true;
-            webView2Control.CreationProperties = null;
-            webView2Control.DefaultBackgroundColor = Color.White;
-            webView2Control.Location = new Point(334, 80);
-            webView2Control.Name = "webView2Control";
-            webView2Control.Size = new Size(583, 358);
-            webView2Control.TabIndex = 2;
-            webView2Control.ZoomFactor = 1D;
             // 
             // menuStrip1
             // 
@@ -72,7 +63,8 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(1391, 28);
+            menuStrip1.Padding = new Padding(8, 2, 0, 2);
+            menuStrip1.Size = new Size(1739, 33);
             menuStrip1.TabIndex = 3;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -80,37 +72,36 @@
             // 
             fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, openRaceBoxFileToolStripMenuItem });
             fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Size = new Size(54, 29);
             fileToolStripMenuItem.Text = "File";
             // 
             // openToolStripMenuItem
             // 
             openToolStripMenuItem.Name = "openToolStripMenuItem";
-            openToolStripMenuItem.Size = new Size(216, 26);
+            openToolStripMenuItem.Size = new Size(270, 34);
             openToolStripMenuItem.Text = "Open";
             openToolStripMenuItem.Click += openToolStripMenuItem_Click;
             // 
             // openRaceBoxFileToolStripMenuItem
             // 
             openRaceBoxFileToolStripMenuItem.Name = "openRaceBoxFileToolStripMenuItem";
-            openRaceBoxFileToolStripMenuItem.Size = new Size(216, 26);
+            openRaceBoxFileToolStripMenuItem.Size = new Size(270, 34);
             openRaceBoxFileToolStripMenuItem.Text = "Open RaceBox File";
             openRaceBoxFileToolStripMenuItem.Click += openRaceBoxFileToolStripMenuItem_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1391, 450);
-            Controls.Add(webView2Control);
+            ClientSize = new Size(1739, 562);
             Controls.Add(listBox2);
             Controls.Add(listBox1);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
+            Margin = new Padding(4, 4, 4, 4);
             Name = "Form1";
             Text = "Form1";
             Load += frmMain_Load;
-            ((System.ComponentModel.ISupportInitialize)webView2Control).EndInit();
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
             ResumeLayout(false);
@@ -121,7 +112,6 @@
 
         private ListBox listBox1;
         private ListBox listBox2;
-        private Microsoft.Web.WebView2.WinForms.WebView2 webView2Control;
         private MenuStrip menuStrip1;
         private ToolStripMenuItem fileToolStripMenuItem;
         private ToolStripMenuItem openToolStripMenuItem;
